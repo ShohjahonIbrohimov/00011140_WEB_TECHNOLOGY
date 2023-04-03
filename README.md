f8da402f92d116f4f1b0a8542dcf538a24b2c329
ibrokhimov06 <ibrokhimov06@gmail.com>
2023-04-03 16:52:36 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Student Mangament system
+
+This is a student managementn system where students can be added, deleted, updated
+
+#### How to run the project
+
+Step 1
+
+```
+npm install
+```
+
+Step 2
+
+```
+npm start
+```
+
+#### Dependency list
+
+axios
+nodemon
+ejs
+notyf
+body-parser
+mongoose
+express
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+var notyf = new Notyf({
+  position: {
+    y: "top",
+    x: "right",
+  },
+});
+
+$("#add_user").submit(function (event) {
+  location.replace("/");
+});
+
+$("#update_user").submit(function (event) {
+  event.preventDefault();
+
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
+
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+
+  var request = {
+    url: `http://localhost:3000/api/users/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (response) {
+    notyf.success("Data Updated Successfully!");
+  });
+});
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+
+    var request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    $.ajax(request).done(function (response) {
+      location.reload();
+    });
+  });
+}
